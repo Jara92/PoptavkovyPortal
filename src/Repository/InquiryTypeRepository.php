4ad862a833ca62e2bafa61fafd1fcc1929c6537e
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2021-11-09 23:46:08 +0100

    Add InquiryType entity, repository and interface.

---
+++ src/Repository/InquiryTypeRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\InquiryType;
+use App\Repository\Interfaces\IInquiryTypeRepository;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method InquiryType|null find($id, $lockMode = null, $lockVersion = null)
+ * @method InquiryType|null findOneBy(array $criteria, array $orderBy = null)
+ * @method InquiryType[]    findAll()
+ * @method InquiryType[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class InquiryTypeRepository extends ServiceEntityRepository implements IInquiryTypeRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, InquiryType::class);
+    }
+
+    // /**
+    //  * @return InquiryType[] Returns an array of InquiryType objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('i.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?InquiryType
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
---
+++ src/Repository/Interfaces/IInquiryTypeRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repository\Interfaces;
+
+use App\Entity\InquiryType;
+
+/**
+ * @implements IRepository<InquiryType, int>
+ */
+interface IInquiryTypeRepository extends IRepository
+{
+
+}
