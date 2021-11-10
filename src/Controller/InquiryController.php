a1f5e0b4d3c1a5225415b893a16a28d351b77f74
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2021-11-10 01:07:29 +0100

    Add first route and setup base inquiry controller actions.

--- src/Controller/InquiryController.php
+++ src/Controller/InquiryController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Services\InquiryService;
+use \Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class InquiryController extends AbstractController
 {
@@ -12,5 +14,25 @@ class InquiryController extends AbstractController
     public function __construct(InquiryService $inquiryService)
     {
         $this->inquiryService = $inquiryService;
+    }
+
+    /**
+     * Show inquiries list.
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->render("layouts/app.html.twig");
+    }
+
+    /**
+     * Show inquiry detail page.
+     * @param Request $request
+     * @param string $alias
+     * @return Response
+     */
+    public function detail(Request $request, string $alias)
+    {
+
     }
 }
