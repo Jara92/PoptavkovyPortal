e71c87e46218c26a8a626b48b64320b73e480c38
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2021-11-17 04:24:46 +0100

    Add inquiry template. Add inquiry Create action and inquiry form.

--- src/Controller/InquiryController.php
+++ src/Controller/InquiryController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Inquiry;
+use App\Form\InquiryForm;
 use App\Services\InquiryService;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use \Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +17,12 @@ use Symfony\Component\HttpFoundation\Response;
 class InquiryController extends AbstractController
 {
     protected $inquiryService;
+    protected $translator;
 
-    public function __construct(InquiryService $inquiryService)
+    public function __construct(InquiryService $inquiryService, TranslatorInterface $translator)
     {
         $this->inquiryService = $inquiryService;
+        $this->translator = $translator;
     }
 
     /**
@@ -22,7 +31,9 @@ class InquiryController extends AbstractController
      */
     public function index()
     {
-        return $this->render("inquiries/index.html.twig");
+        $inquiries = $this->inquiryService->readAll();
+
+        return $this->render("inquiry/index.html.twig", ["inquiries" => $inquiries]);
     }
 
     /**
@@ -32,7 +43,7 @@ class InquiryController extends AbstractController
      */
     public function detail(string $alias)
     {
-        return $this->render("inquiries/detail.html.twig");
+        return $this->render("inquiry/detail.html.twig");
     }
 
     /**
@@ -41,7 +52,12 @@ class InquiryController extends AbstractController
      */
     public function create(Request $request)
     {
-        return $this->render("inquiries/create.html.twig");
+        $inquiry = new Inquiry();
+
+        $form = $this->createForm(InquiryForm::class, $inquiry);
+
+        // Same as $this->render('...', ['form' => $form->createView()])
+        return $this->renderForm("inquiry/create.html.twig", ["form" => $form]);
     }
 
     /**
@@ -52,7 +68,7 @@ class InquiryController extends AbstractController
      */
     public function edit(Request $request, string $alias)
     {
-        return $this->render("inquiries/edit.html.twig");
+        return $this->render("inquiry/edit.html.twig");
     }
 
 
---
+++ src/Form/InquiryForm.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Inquiry;
+use App\Entity\Region;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InquiryForm extends AbstractType
+{
+    protected $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Inquiry::class,
+        ]);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'label' => $this->translator->trans("field_title", [], "inquiries")
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => $this->translator->trans("field_description", [], "inquiries")
+            ])
+            ->add("region", EntityType::class, [
+                // looks for choices from this entity
+                'class' => Region::class,
+
+                // uses the User.username property as the visible option string
+                'choice_label' => 'title',
+
+                // used to render a select box, check boxes or radios
+                // 'multiple' => true,
+                // 'expanded' => true,
+            ])
+            ->add("contactEmail", EmailType::class, [
+                'label' => $this->translator->trans("field_email", [], "inquiries")
+            ])
+            // TODO: Create a custom type for a phone number.
+            ->add("contactPhone", TextType::class, [
+                'label' => $this->translator->trans("field_phone", [], "inquiries")
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => $this->translator->trans("btn_submit", [], "inquiries")
+            ])
+            ->getForm();
+    }
+}
