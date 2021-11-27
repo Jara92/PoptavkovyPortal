c47368100194a02fc48f992f69731986ddc5163d
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2021-11-27 05:28:03 +0100

    Prepare Inquiry form. Add GDPR and terms checkboxes. Improve style.

--- src/Form/InquiryForm.php
+++ src/Form/InquiryForm.php
@@ -7,10 +7,14 @@ use App\Entity\Inquiry;
 use App\Entity\InquiryType;
 use App\Entity\InquiryValue;
 use App\Entity\Region;
+use App\Repository\InquiryTypeRepository;
+use App\Repository\Interfaces\IInquiryTypeRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -21,16 +25,19 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class InquiryForm extends AbstractType
 {
     protected $translator;
+    protected $inquiryRepository;
 
-    public function __construct(TranslatorInterface $translator)
+    public function __construct(TranslatorInterface $translator, IInquiryTypeRepository $inquiryTypeRepository)
     {
         $this->translator = $translator;
+        $this->inquiryTypeRepository = $inquiryTypeRepository;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => Inquiry::class,
+            'translation_domain' => "inquiries"
         ]);
     }
 
@@ -39,15 +46,21 @@ class InquiryForm extends AbstractType
         $builder
             ->add('title', TextType::class, [
                 'required' => true,
-                'label' => $this->translator->trans("field_title", [], "inquiries")
+                'label' => "field_title",
+                'attr' => [
+                    "placeholder" => "field_title_ph",
+                ]
             ])
             ->add('description', TextareaType::class, [
                 'required' => true,
-                'label' => $this->translator->trans("field_description", [], "inquiries")
+                'label' => "field_description",
+                'attr' => [
+                    "placeholder" => "field_description_ph",
+                ]
             ])
             ->add("region", EntityType::class, [
                 'required' => true,
-                'label' => $this->translator->trans("field_region", [], "inquiries"),
+                'label' => "field_region",
                 // looks for choices from this entity
                 'class' => Region::class,
 
@@ -62,7 +75,7 @@ class InquiryForm extends AbstractType
             ])
             ->add("value", EntityType::class, [
                 'required' => true,
-                'label' => $this->translator->trans("field_value", [], "inquiries"),
+                'label' => "field_value",
                 // looks for choices from this entity
                 'class' => InquiryValue::class,
 
@@ -77,7 +90,7 @@ class InquiryForm extends AbstractType
             ])
             ->add("deadline", EntityType::class, [
                 'required' => true,
-                'label' => $this->translator->trans("field_deadline", [], "inquiries"),
+                'label' => "field_deadline",
                 // looks for choices from this entity
                 'class' => Deadline::class,
 
@@ -93,27 +106,76 @@ class InquiryForm extends AbstractType
             ->add("type", EntityType::class, [
                 'required' => true,
                 'label' => false,
+
+                'data' => $this->inquiryTypeRepository->findOneByAlias("personal"),
                 // looks for choices from this entity
                 'class' => InquiryType::class,
 
                 // uses the User.username property as the visible option string
                 'choice_label' => 'title',
 
-                'translation_domain' => "inquiries",
                 'choice_translation_domain' => "inquiries",
                 // used to render a select box, check boxes or radios
                 // 'multiple' => true,
                 'expanded' => true,
+            ])->add($builder->create('personalInquiry', FormType::class, [
+                'translation_domain' => "inquiries",
+                'by_reference' => false
+            ])->add('name', TextType::class, [
+                'label' => "field_individual_name",
+                'attr' => [
+                    "placeholder" => "field_individual_name_ph",
+                ],
+                'required' => true,
             ])
+                ->add('surname', TextType::class, [
+                    'label' => "field_individual_surname",
+                    'attr' => [
+                        "placeholder" => "field_individual_surname_ph",
+                    ],
+                    'required' => true
+                ])
+            )
+            ->add($builder->create('companyInquiry', FormType::class, [
+                'by_reference' => false
+            ])
+                ->add('name', TextType::class, [
+                    'label' => "field_firm_name",
+                    'attr' => [
+                        "placeholder" => "field_firm_name_ph",
+                    ],
+                    'required' => true
+                ])
+            )
             ->add("contactEmail", EmailType::class, [
-                'label' => $this->translator->trans("field_email", [], "inquiries")
+                'label' => "field_email",
+                'attr' => [
+                    "placeholder" => "field_email_ph",
+                ],
             ])
             // TODO: Create a custom type for a phone number.
             ->add("contactPhone", TextType::class, [
-                'label' => $this->translator->trans("field_phone", [], "inquiries")
+                'required' => false,
+                'label' => "field_phone",
+                'attr' => [
+                    "placeholder" => "field_phone_ph",
+                ],
             ])
             ->add('submit', SubmitType::class, [
-                'label' => $this->translator->trans("btn_submit", [], "inquiries")
+                'label' => "btn_submit",
+            ])
+            ->add("gdpr", CheckboxType::class, [
+                'label' => "field_gdpr_agree",
+                'required' => true,
+                // This is an extra field which is not represented in Inquiry entity.
+                'mapped' => false,
+            ])
+            ->add("terms", CheckboxType::class, [
+                'label' => "field_terms_agree",
+                'label_html' => true,
+                'required' => true,
+                // This is an extra field which is not represented in Inquiry entity.
+                'mapped' => false
             ])
             ->getForm();
     }
