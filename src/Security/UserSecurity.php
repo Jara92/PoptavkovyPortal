72e75972a0e3868b5cf3cc2d3b1b089ece0b5fe0
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2022-02-06 21:59:29 +0100

    Fix: delete Security class inheritance.

--- src/Security/UserSecurity.php
+++ src/Security/UserSecurity.php
@@ -3,17 +3,21 @@
 namespace App\Security;
 
 use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
 
-class UserSecurity extends Security
+class UserSecurity
 {
+    /** @required  */
+    public Security $security;
+
     /**
      * Returns currently logged user or null.
      * @return User|null
      */
     public function getUser(): ?User
     {
-        $user = parent::getUser();
+        $user = $this->security->getUser();
 
         // Is user valid?
         if ($user instanceof User) {
@@ -29,6 +33,17 @@ class UserSecurity extends Security
      */
     public function isLoggedIn(): bool
     {
-        return $this->getUser() !== null;
+        return $this->security->getUser() !== null;
+    }
+
+    /**
+     * Checks if the attributes are granted against the current authentication token and optionally supplied subject.
+     */
+    public function isGranted(mixed $attributes, mixed $subject = null): bool{
+        return $this->security->isGranted($attributes, $subject);
+    }
+
+    public function getToken(): ?TokenInterface{
+        return $this->security->getToken();
     }
 }
