8ba0fe51b8bce38a1848d92af50fd3658da250b0
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2022-02-09 16:27:31 +0100

    Refactor: Select2 features in inquiry-filter_controller.js

--- assets/controllers/inquiry-filter_controller.js
+++ assets/controllers/inquiry-filter_controller.js
@@ -7,49 +7,66 @@ import {Controller} from '@hotwired/stimulus';
 export default class extends Controller {
     static targets = ["categories", "regions"];
 
+    select2GlobalOptions = {};
+
     constructor(context) {
         super(context);
 
-        console.log(this.regionsTarget);
+        this.initSelect2GlobalOptions();
 
-        jQuery(this.regionsTarget);
-
-        //Set Dropdown with SearchBox via dropdownAdapter option (https://stackoverflow.com/questions/35799854/how-to-add-selectall-using-dropdownadapter-on-select2-v4)
-        var Utils = $.fn.select2.amd.require('select2/utils');
-        var Dropdown = $.fn.select2.amd.require('select2/dropdown');
-        var DropdownSearch = $.fn.select2.amd.require('select2/dropdown/search');
-        var CloseOnSelect = $.fn.select2.amd.require('select2/dropdown/closeOnSelect');
-        var AttachBody = $.fn.select2.amd.require('select2/dropdown/attachBody');
-
-        var dropdownAdapter = Utils.Decorate(Utils.Decorate(Utils.Decorate(Dropdown, DropdownSearch), CloseOnSelect), AttachBody);
-
-        function showSelectedNumber(obj) {
-            var uldiv = $(obj).siblings('span.select2').find('ul');
-            var count = $(obj).select2('data').length;
-            if (count === 0) {
-                uldiv.html("");
-            } else {
-                uldiv.addClass("p-2");
-                uldiv.html("" + count + " položek vybráno" + "");
-            }
-        }
-
-        jQuery(document).ready(function () {
-            let regions = jQuery('#inquiry_filter_form_regions').select2({
-                dropdownAdapter: dropdownAdapter,
-                placeholder: "Zvolte možnost",
-                multiple: true,
-                closeOnSelect: false,
-                selectionCssClass: "form-control"
-            }).on('select2:opening select2:closing', function (event) {
-                //Disable original search (https://select2.org/searching#multi-select)
-                var searchfield = jQuery(this).parent().find('.select2-search__field');
-                searchfield.prop('disabled', true);
-            }).on('select2:close', function (evt) {
-                showSelectedNumber(this);
-            });
-
-            showSelectedNumber(regions);
+        let regions = this.prepareSelect2Field(this.regionsTarget, {
+            multiple: true
         });
+    }
+
+    getDropdownAdapter() {
+        //Set Dropdown with SearchBox via dropdownAdapter option (https://stackoverflow.com/questions/35799854/how-to-add-selectall-using-dropdownadapter-on-select2-v4)
+        let Utils = $.fn.select2.amd.require('select2/utils');
+        let Dropdown = $.fn.select2.amd.require('select2/dropdown');
+        let DropdownSearch = $.fn.select2.amd.require('select2/dropdown/search');
+        let CloseOnSelect = $.fn.select2.amd.require('select2/dropdown/closeOnSelect');
+        let AttachBody = $.fn.select2.amd.require('select2/dropdown/attachBody');
+
+        return Utils.Decorate(Utils.Decorate(Utils.Decorate(Dropdown, DropdownSearch), CloseOnSelect), AttachBody);
+    }
+
+    initSelect2GlobalOptions() {
+        let dropdownAdapter = this.getDropdownAdapter();
+
+        this.select2GlobalOptions = {
+            dropdownAdapter: dropdownAdapter,
+            closeOnSelect: false,
+            selectionCssClass: "form-control",
+            placeholder: "Zvolte možnost"
+        };
+    }
+
+    prepareSelect2Field(elem, options = {}) {
+        let controller = this;
+
+        // Init Select2 object using global and local options.
+        let selection = jQuery(elem).select2({
+            ...this.select2GlobalOptions,
+            ...options
+        }).on('select2:opening select2:closing', function (event) {
+            //Disable original search (https://select2.org/searching#multi-select)
+            var searchfield = jQuery(this).parent().find('.select2-search__field');
+            searchfield.prop('disabled', true);
+        }).on('select2:close', function (evt) {
+            controller.showSelectedNumber(this);
+        });
+
+        return selection;
+    }
+
+    showSelectedNumber(obj) {
+        var uldiv = $(obj).siblings('span.select2').find('ul');
+        var count = $(obj).select2('data').length;
+        if (count === 0) {
+            uldiv.html("");
+        } else {
+            uldiv.addClass("p-2");
+            uldiv.html("" + count + " položek vybráno" + "");
+        }
     }
 }
