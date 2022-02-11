876ad7f23d7eb906a0e8e537ba7b16a066b50e21
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2022-02-11 19:14:17 +0100

    Add InquiryCategoryService and readAllRootCategories() method

---
+++ src/Business/Service/InquiryCategoryService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Business\Service;
+
+use App\Entity\Inquiry\Inquiry;
+use App\Entity\Inquiry\InquiryCategory;
+use App\Repository\Interfaces\IInquiryCategoryRepository;
+use App\Repository\Interfaces\IRepository;
+
+/**
+ * Service class which handles everything about InquiryCategories.
+ * @extends  AService<InquiryCategory, int>
+ */
+class InquiryCategoryService extends AService
+{
+    /** @var IInquiryCategoryRepository */
+    public IRepository $repository;
+
+    public function __construct(IInquiryCategoryRepository $categoryRepository)
+    {
+        parent::__construct($categoryRepository);
+    }
+
+    public function readByAlias(string $alias): ?Inquiry
+    {
+        return $this->repository->findOneBy(["alias" => $alias]);
+    }
+
+    /**
+     * Returns all categories without a parent.
+     * @param array|string[] $orderBy
+     * @return InquiryCategory[]
+     */
+    public function readAllRootCategories(array $orderBy = ["title" => "ASC"]): array
+    {
+        return $this->repository->findBy(["parent" => null], $orderBy);
+    }
+}
