ca2e76cf2ad76a60164caacc1d881921c4e31b90
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2022-02-11 21:01:25 +0100

    Add UrlHelperTest.php

---
+++ tests/Unit/src/Helper/UrlHelperTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Tests\Unit\src\Helper;
+
+use App\Helper\UrlHelper;
+use PHPUnit\Framework\TestCase;
+
+class UrlHelperTest extends TestCase
+{
+    public function testCreateAlias(): void
+    {
+        $title = ["Toto je Titulek.", "ěščřžýáíé", "Lomítka/jsou/super!", "12345", "--111ahoj???ahoj--", "&ahoj=1", "<script>aa</script>"];
+        $alias = ["toto-je-titulek", "escrzyaie", "lomitka-jsou-super", "12345", "111ahoj-ahoj", "ahoj-1", "script-aa-script"];
+
+        for ($i = 0; $i < count($title); $i++) {
+            $this->assertSame($alias[$i], UrlHelper::createAlias($title[$i]));
+        }
+    }
+
+    public function testCreateAliasEdgeCases(): void
+    {
+        $title = [""];
+        $alias = ["n-a"];
+
+        for ($i = 0; $i < count($title); $i++) {
+            $this->assertSame($alias[$i], UrlHelper::createAlias($title[$i]));
+        }
+    }
+
+    public function testCreateIdAlias(): void
+    {
+        $id = [1, -1];
+        $title = ["Titulek moji stranky", "Test"];
+        $alias = ["1-titulek-moji-stranky", "1-test"];
+
+        for ($i = 0; $i < count($title); $i++) {
+            $this->assertSame($alias[$i], UrlHelper::createIdAlias($id[$i], $title[$i]));
+        }
+    }
+}
