c3592c6a1be946568b5982d4f75cf9c03bb61ff7
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2022-02-11 23:10:38 +0100

    Add AService test

---
+++ tests/Unit/Business/Service/AServiceTest.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Tests\Unit\Business\Service;
+
+use App\Business\Service\AService;
+use App\Entity\Inquiry\Inquiry;
+use App\Repository\Interfaces\IRepository;
+use PHPUnit\Framework\TestCase;
+
+class AServiceTest extends TestCase
+{
+    /** IRepository mock */
+    protected IRepository $repository;
+
+    protected AService $service;
+
+    protected Inquiry $entity;
+    protected array $entities;
+
+    function setUp(): void
+    {
+        parent::setUp();
+
+        $this->repository = $this->createStub(IRepository::class);
+        $this->service = new AService($this->repository);
+
+        $this->entity = (new Inquiry())->setId(13);
+        $this->entities = [
+            (new Inquiry())->setId(1)->setTitle("Test"),
+            (new Inquiry())->setId(2)->setTitle("Test 2")
+        ];
+    }
+
+    public function testReadById(): void
+    {
+        $entity= $this->entity;
+
+        // Id to be used
+        $id = $entity->getId();
+
+        // Mock repository find method - should return the entity.
+        $this->repository->expects($this->once())->method("find")->with($id)->willReturn($entity);
+
+        $this->assertEquals($entity, $this->service->readById($id));
+    }
+
+    public function testReadByIdNotExist(): void
+    {
+        // Id to be used
+        $id = 23;
+
+        // Mock repository find method - should return null because the ID does not exist.
+        $this->repository->expects($this->once())->method("find")->with($id)->willReturn(null);
+
+        $this->assertNull($this->service->readById($id));
+    }
+
+    public function testReadAll():void{
+        $entities = $this->entities;
+
+        // Mock repository find method - should return null because the ID does not exist.
+        $this->repository->expects($this->once())->method("findAll")->willReturn($entities);
+
+        $this->assertEquals($entities, $this->service->readAll());
+    }
+
+    public function testReadBy():void{
+        $entities = [$this->entities[0]];
+        $params = ["title" => "Test"];
+
+        // Mock repository find method - should return null because the ID does not exist.
+        $this->repository->expects($this->once())->method("findBy")->with($params)->willReturn($entities);
+
+        $this->assertEquals($entities, $this->service->readBy($params));
+    }
+
+    public function testExistsById():void{
+        $id = $this->entity->getId();
+        $this->repository->expects($this->once())->method("find")->with($id)->willReturn($this->entity);
+
+        $this->assertEquals(true, $this->service->existsById($id));
+    }
+
+    public function testExistsByIdIdNotExist():void{
+        $id = 23;
+        $this->repository->expects($this->once())->method("find")->with($id)->willReturn(null);
+
+        $this->assertEquals(false, $this->service->existsById($id));
+    }
+}
