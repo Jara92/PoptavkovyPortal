622b630276e4b93f728e432975dc7cfa216dfd9e
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2022-02-15 00:21:24 +0100

    Add new icons

--- assets/app.js
+++ assets/app.js
@@ -27,6 +27,7 @@ import './bootstrap';
 // Font awesome
 import "@fortawesome/fontawesome-free/js/brands";
 import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/fontawesome";
 
 
