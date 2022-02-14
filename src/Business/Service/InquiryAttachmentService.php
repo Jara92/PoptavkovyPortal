fe8db4a7edf74b7955d60148759b50086e442483
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2022-02-14 19:26:25 +0100

    Add InquiryAttachmentService

---
+++ src/Business/Service/InquiryAttachmentService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Business\Service;
+
+use App\Entity\Inquiry\InquiryAttachment;
+use App\Repository\Interfaces\IRepository;
+use App\Repository\Interfaces\IInquiryAttachmentRepository;
+
+/**
+ * Service class which handles Inquiry attachments.
+ * @extends  AService<InquiryAttachment, int>
+ */
+class InquiryAttachmentService extends AService
+{
+    /** @var IInquiryAttachmentRepository */
+    public IRepository $repository;
+
+    public function __construct(IInquiryAttachmentRepository $attachmentRepository)
+    {
+        parent::__construct($attachmentRepository);
+    }
+}
