6b86733ed3a0ee487a9a520b1daa39076427a22f
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2022-02-22 21:44:01 +0100

    Add "translationCases()" method into InquiryType

--- src/Enum/Entity/InquiryType.php
+++ src/Enum/Entity/InquiryType.php
@@ -6,4 +6,16 @@ enum InquiryType: string
 {
     case PERSONAL = "personal";
     case COMPANY = "company";
+
+    /**
+     * Returns array [translation_key => InquiryType->value]
+     * @return array
+     */
+    public static function translationCases(): array
+    {
+        return [
+            "inquiry_type.personal" => InquiryType::PERSONAL->value,
+            "inquiry_type.company" => InquiryType::COMPANY->value,
+        ];
+    }
 }
