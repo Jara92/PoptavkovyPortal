bd551d5c831691f2c27dc3c5ad397a5c0d150fc5
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2022-02-27 22:46:42 +0100

    Fix email template path

--- src/Controller/Auth/ResetPasswordController.php
+++ src/Controller/Auth/ResetPasswordController.php
@@ -163,7 +163,7 @@ class ResetPasswordController extends AbstractController
             ->from(new Address('info@poptejsi.cz', 'Poptejsi.cz'))
             ->to($user->getEmail())
             ->subject('Your password reset request')
-            ->htmlTemplate('reset_password/email.html.twig')
+            ->htmlTemplate('email/auth/reset_password.html.twig')
             ->context([
                 'resetToken' => $resetToken,
             ]);
