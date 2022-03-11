f404e3c3e939b20bac41a2a7e1fca05878ce4ffb
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2022-03-11 18:35:51 +0100

    Fix IInquirySignedRequestRepository.php

--- src/Repository/Interfaces/Inquiry/IInquirySignedRequestRepository.php
+++ src/Repository/Interfaces/Inquiry/IInquirySignedRequestRepository.php
@@ -2,12 +2,11 @@
 
 namespace App\Repository\Interfaces\Inquiry;
 
-use App\Entity\Inquiry\CompanyContact;
-use App\Entity\Inquiry\Inquiry;
+use App\Entity\Inquiry\InquirySignedRequest;
 use App\Repository\Interfaces\IRepository;
 
 /**
- * @implements IRepository<Inquiry, int>
+ * @implements IRepository<InquirySignedRequest, int>
  */
 interface IInquirySignedRequestRepository extends IRepository
 {
