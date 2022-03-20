7327252c03bcb600a4c84efa1d4ccc6b99684441
Jaroslav Fikar <fikarja3@fit.cvut.cz>
2022-03-20 19:06:07 +0100

    Add breadcrumbs on article pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,17 +5,26 @@ namespace App\Controller;
 use App\Business\Service\Inquiry\InquiryCategoryService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use WhiteOctober\BreadcrumbsBundle\Model\Breadcrumbs;
 
 class HomeController extends AbstractController
 {
     public function __construct(
         private InquiryCategoryService $inquiryCategoryService,
+        private Breadcrumbs            $breadcrumbs,
+        private RouterInterface        $router
     )
     {
+        $this->breadcrumbs->addItem("mainnav.home", $this->router->generate("home"));
+        $this->breadcrumbs->addItem("articles.how_it_works", $this->router->generate("how-it-works"));
     }
 
     public function home(): Response
     {
+        // No breadcrumbs on home page
+        $this->breadcrumbs->clear();
+
         $categories = $this->inquiryCategoryService->readAllRootCategories();
 
         return $this->render("home/home.html.twig", compact("categories"));
@@ -23,11 +32,15 @@ class HomeController extends AbstractController
 
     public function inquiring(): Response
     {
+        $this->breadcrumbs->addItem("articles.for_inquiring", $this->router->generate("inquiring"));
+
         return $this->render("home/inquiring.html.twig");
     }
 
     public function supplier(): Response
     {
+        $this->breadcrumbs->addItem("articles.suppliers", $this->router->generate("suppliers"));
+
         return $this->render("home/suppliers.html.twig");
     }
 
